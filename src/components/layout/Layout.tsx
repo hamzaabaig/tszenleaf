f7e015340c3194f21d51154a6167937c3417597e
sohaib.hassan@mergestack.com <ishfaq.ahmed@mergestack.com>
2024-11-25 19:09:32 +0500

    Create Hero component and Define color palette

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Wrapper } from "./style";
 import BlogHeader from "../blogHeader/BlogHeader";
+import HeroSection from "../hero/HeroSection";
 
 const Layout = () => {
   return (
     <Wrapper>
       <BlogHeader />
+      <HeroSection />
     </Wrapper>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const { color } = require("./src/color");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: { colors: color },
   },
   plugins: [],
 };
