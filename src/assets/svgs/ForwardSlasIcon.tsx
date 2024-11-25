699c3df261548da46ddcfbd22adf79622ac1a424
sohaib.hassan@mergestack.com <ishfaq.ahmed@mergestack.com>
2024-11-25 18:59:53 +0500

    Create resuable components for svgs

---
+++ src/assets/svgs/ForwardSlasIcon.tsx
@@ -0,0 +1,16 @@
+
+const ForwardSlashIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z"></path>
+    </svg>
+  );
+};
+
+export default ForwardSlashIcon;
---
+++ src/assets/svgs/RightArrowIcon.tsx
@@ -0,0 +1,19 @@
+
+const RightArrowIcon = ({ className }: { className: string }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 5L16 12L9 19"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
+
+export default RightArrowIcon;
