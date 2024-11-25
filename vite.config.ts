b02b60fff4389dcdc40c509762201e9cf17aaf68
sohaib.hassan@mergestack.com <ishfaq.ahmed@mergestack.com>
2024-11-25 17:59:39 +0500

    Project setup

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import macrosPlugin from 'vite-plugin-babel-macros'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  esbuild: {
+    jsxFactory: 'jsx',
+    jsxInject: 'import { jsx } from "@emotion/react"',
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'babel-plugin-macros',
+          [
+            '@emotion/babel-plugin-jsx-pragmatic',
+            {
+              export: 'jsx',
+              import: '__cssprop',
+              module: '@emotion/react',
+            },
+          ],
+          [
+            '@babel/plugin-transform-react-jsx',
+            { pragma: '__cssprop' },
+            'twin.macro',
+          ],
+        ],
+      },
+    }),
+    macrosPlugin(),
+  ],
+  define: {
+    'process.env': {},
+  },
+})
