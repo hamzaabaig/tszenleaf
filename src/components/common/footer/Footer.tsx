ff18bf35c36bdc68334924710fc9bb2f922db1ae
sohaib.hassan@mergestack.com <ishfaq.ahmed@mergestack.com>
2024-11-27 13:25:22 +0500

    Create and style Footer component

---
+++ src/components/common/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import { Wrapper } from "./style"
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <p className="font-semibold">
+        Copyright &copy; 2024 Zen Leaf
+      </p>
+    </Wrapper>
+  )
+}
+
+export default Footer
---
+++ src/components/common/footer/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Wrapper = styled.div`
+  ${tw`bg-primary p-4 text-white text-center`}
+`;
+
+export { Wrapper };
