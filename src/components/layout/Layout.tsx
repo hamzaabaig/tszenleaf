229d503083a3a5d551397990205892d2445bc4eb
sohaib.hassan@mergestack.com <ishfaq.ahmed@mergestack.com>
2024-11-27 13:37:00 +0500

    Update Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,17 +4,21 @@ import HeroSection from "../hero/HeroSection";
 import Slider from "../slider/Slider";
 import BlogArchive from "../blogArchive/BlogArchive";
 import LeftSidebar from "../common/leftSidebar/LeftSidebar";
+import Footer from "../common/footer/Footer";
 
 const Layout = () => {
   return (
     <Wrapper>
-      <BlogHeader />
-      <HeroSection />
-      <Slider />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
-        <LeftSidebar />
-        <BlogArchive />
+      <div className="px-3 lg:px-10 ">
+        <BlogHeader />
+        <HeroSection />
+        <Slider />
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
+          <LeftSidebar />
+          <BlogArchive />
+        </div>
       </div>
+      <Footer />
     </Wrapper>
   );
 };
